ed883cd2c3e10f411bf4ea8fb04a1840fec3fa2e
Eugenia Musat <musat.eugenia@gmail.com>
2022-04-22 22:43:14 +0200

    Create /about route
    Create route in app.py
    Create about template
    Correct links to about url

--- app.py
+++ app.py
@@ -15,5 +15,13 @@ def home_page():
     """
     return render_template("home_page.html")
 
+@app.route("/about")
+def about():
+    """
+    This route displays the about webpage.
+    """
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=os.environ.get("DEBUG", False))
